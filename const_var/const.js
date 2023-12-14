47a5381f1ff20774155ebadd1679bee2645bf6d4
zoro2409 <94405980+zoro2409@users.noreply.github.com>
2023-12-14 00:12:06 +0000

    let_const_var

---
+++ const_var/const.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "nihalrajora@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+let accountState;
+
+console.table([accountId , accountEmail ,accountPassword ,accountState ]);
+
+accountEmail = "rajoranihal@gmail.com"
+accountPassword = "123456"
+accountCity = "ChittorGarh"
+
+console.table([accountId , accountEmail ,accountPassword ,accountState ]);
